6fc6049f2e832d077aa3588ed0433fc343bf161e
Victor Navas <vnavas@gmail.com>
2011-06-14 20:29:07 +0200

    genzai

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Genzai::Application.config.secret_token = '87dd363306e0b9562f8a72973b2dfc8a3ac3f43983fcc4d591c3a2ac0bf4c8c7b2f61b9a5b84a1296a77a3c12dd434b6c1b6000751bc768a5e2fc514660a6ba6'
